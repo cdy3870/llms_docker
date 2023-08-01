ab3257ad69d218eb6da383cb3fc7ffa64225fee5
Calvin Yu <cdy3870@gmail.com>
2023-08-01 18:42:43 -0400

    added open ai api work

--- api_stuff/fast_api_test.py
+++ api_stuff/fast_api_test.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI, HTTPException
 from joblib import load
 from pydantic import BaseModel
+# from transformers import pipeline
+import json
+import os
+from langchain.llms import OpenAI
+from langchain.agents import load_tools, initialize_agent, AgentType
 
-# # Load the model
-# spam_clf = load(open('./models/spam_detector_model.pkl','rb'))
+# classifier = pipeline("zero-shot-classification",
+#                       model="facebook/bart-large-mnli")
 
+key = os.getenv("OPENAI_API_KEY")
+llm = OpenAI(openai_api_key=key)
+tools = load_tools(
+	["arxiv"],
+)
+
+agent_chain = initialize_agent(tools, llm, 
+								agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
 
 # Initialize an instance of FastAPI
 app = FastAPI()
 
-class Item(BaseModel):
-    text_message: str
-    another_field: str
 
 # Define the default route 
 @app.get("/")
@@ -19,9 +29,16 @@ def root():
 	return {"message": "Welcome to Your Sentiment Classification FastAPI"}
 
 
+class Item(BaseModel):
+    text_input: str
+    candidate_labels: str
+
 # Define the route to the sentiment predictor
 @app.post("/predict_sentiment")
-def predict_sentiment(results: Item):
+def predict_sentiment(item: Item):
+	labels = item.candidate_labels.split(" ")
+	prediction = classifier(item.text_input, labels)
+
 
 	# polarity = ""
 
@@ -36,7 +53,44 @@ def predict_sentiment(results: Item):
 
 	# elif(prediction[0] == 1):
 	#     polarity = "Spam"
-	
-	polarity = "Spam"
 
-	return results
+	return prediction
+
+
+class DOI(BaseModel):
+    text: str
+
+@app.post("/get_summary")
+def get_summary(doi: DOI):
+	response = agent_chain.run(f"What is the paper {doi.text} about?")
+
+	print(response)
+
+	return response
+
+@app.post("/get_con")
+def get_con(doi: DOI):
+	response = agent_chain.run(f"What are the main contributions of {doi.text} as a bullet points?")
+
+	print(response)
+
+	return response
+
+@app.post("/get_details")
+def get_details(doi: DOI):
+	response = agent_chain.run(f"What year was {doi.text} published and in what conference or journal?")
+
+	print(response)
+
+	return response
+
+@app.post("/get_recs")
+def get_recs(topic: DOI):
+	response = agent_chain.run(f"Can you find a list of papers related to {topic}")
+
+	print(response)
+
+	return response
+
+
+
--- app_stuff/streamlit_app.py
+++ app_stuff/streamlit_app.py
@@ -1,10 +1,53 @@
 import streamlit as st
 import requests
 import json
+import numpy as np
 
-inputs = {"text_message": "test", "another_field": "test2"}
+# text_input = st.text_input("Enter some text")
+# cand_labels = st.text_input("Candidate labels")
 
-if st.button("Run test"):
-	print(inputs)
-	res = requests.post(url="http://api/predict_sentiment", json=inputs)
-	st.subheader(f"{res.text}")
+
+
+# # Get a prediction
+# # sequence_to_classify = "one day I will see the world"
+# # candidate_labels = ['travel', 'cooking', 'dancing']
+# # labels = cand_labels.split(" ")
+
+# inputs = {"text_input": text_input, "candidate_labels": cand_labels}
+
+# if st.button("Get most likely label"):
+# 	res = requests.post(url="http://0.0.0.0:8000/predict_sentiment", json=inputs)
+# 	predictions = json.loads(res.text)
+# 	sequence = predictions["labels"][np.argmax(predictions["scores"])]
+
+# 	st.subheader(f"{sequence}")
+
+
+
+
+
+doi = st.text_input("Enter a research topic (ex. machine learning and drones) for related papers")
+inputs = {"text": doi}
+
+if st.button("Get recommended papers"):
+	res = requests.post(url="http://0.0.0.0:8000/get_recs", json=inputs)
+	st.markdown(res.text)	
+
+doi = st.text_input("Enter an arxiv DOI or paper name")
+inputs = {"text": doi}
+
+if st.button("Get info"):
+	# res = requests.post(url="http://0.0.0.0:8000/get_summary", json=inputs)
+	# st.markdown(f"Summary: {res.text}")
+
+	res = requests.post(url="http://0.0.0.0:8000/get_con", json=inputs)
+
+	bullet_points = res.text.split("\\n-")
+	print(bullet_points)
+
+	st.markdown(f"Contributions:")
+	for i, bullet in enumerate(bullet_points[1:]):
+		st.markdown(f"{i + 1}:{bullet}")
+
+	res = requests.post(url="http://0.0.0.0:8000/get_details", json=inputs)
+	st.markdown(res.text)
