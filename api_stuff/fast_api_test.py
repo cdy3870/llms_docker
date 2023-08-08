b186fd1f78c902e8ab63af82cbd764ebadd94de2
Calvin Yu <cdy3870@gmail.com>
2023-08-08 19:35:38 -0400

    updated code

--- api_stuff/fast_api_test.py
+++ api_stuff/fast_api_test.py
@@ -6,26 +6,15 @@ import json
 import os
 from langchain.llms import OpenAI
 from langchain.agents import load_tools, initialize_agent, AgentType, Tool
-from langchain.utilities import GoogleSerperAPIWrapper
-
-# classifier = pipeline("zero-shot-classification",
-#                       model="facebook/bart-large-mnli")
+import openai
 
 llm = OpenAI()
 arxiv_tools = load_tools(["arxiv"])
 arxiv_agent = initialize_agent(arxiv_tools, llm, 
 								agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
 
-
-google_tools = [
-    Tool(
-        name="Intermediate Answer",
-        func=GoogleSerperAPIWrapper().run,
-        description="useful for when you need to ask with search",
-    )
-]
-
-google_agent = initialize_agent(google_tools, llm, agent=AgentType.SELF_ASK_WITH_SEARCH)
+api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = api_key
 
 # Initialize an instance of FastAPI
 app = FastAPI()
@@ -37,37 +26,8 @@ def root():
 	return {"message": "arXiv backend"}
 
 
-# class Item(BaseModel):
-#     text_input: str
-#     candidate_labels: str
-
-
 class Item(BaseModel):
     text: str
-
-# @app.post("/get_summary")
-# def get_summary(doi: Item):
-# 	response = arxiv_agent.run(f"What is the paper {doi.text} about?")
-
-# 	print(response)
-
-# 	return response
-
-# @app.post("/get_con")
-# def get_con(doi: Item):
-# 	response = arxiv_agent.run(f"What are the main contributions of {doi.text} as a bullet points?")
-
-# 	print(response)
-
-# 	return response
-
-# @app.post("/get_details")
-# def get_details(doi: Item):
-# 	response = arxiv_agent.run(f"Give the conference of the paper: {doi.text}")
-
-# 	print(response)
-
-# 	return response
 
 @app.post("/get_recs")
 def get_recs(topic: Item):
@@ -77,18 +37,8 @@ def get_recs(topic: Item):
 	return response
 
 
-@app.post("/get_h5")
-def get_h5(conf: Item):
-	response = google_agent.run(f"What is the h5 index of the conference {conf.text}?")
 
-	print(response)
-
-	return response
-
-@app.post("/get_im")
-def get_im(conf: Item):
-	response = google_agent.run(f"What is the impact score of the conference {conf.text}?")
-
-	print(response)
-
+@app.post("/get_main_confs")
+def get_main_confs(prompt: Item):
+	response = openai.Completion.create(engine="text-davinci-003", prompt=prompt.text, max_tokens=100,stop=None,temperature=0.7)["choices"][0]["text"]
 	return response
--- app_stuff/streamlit_app.py
+++ app_stuff/streamlit_app.py
@@ -5,18 +5,70 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import string
+import plotly.express as px
 
 st.set_page_config(
 	page_title="Conference Recommender", layout="wide")
 
+st.markdown(
+	"""
+<style>
+[data-testid="stMetricValue"] {
+	font-size: 20px;
+}
+</style>
+""",
+	unsafe_allow_html=True,
+)
 
 def generate_url(paper):
-    URL = "https://arxiv.org/search/?query="
-    for string in paper.split(" "):
-        URL = URL + string + "+"
-    URL = URL[:-1]
-    URL += "&searchtype=all&source=header"
-    return URL
+	URL = "https://arxiv.org/search/?query="
+	for string in paper.split(" "):
+		URL = URL + string + "+"
+	URL = URL[:-1]
+	URL += "&searchtype=all&source=header"
+	return URL
+
+def generate_conf_url(conf):
+	URL = "https://www.scimagojr.com/journalsearch.php?q="
+	print(conf.split(" "))
+	for string in conf.split(" "):
+		URL = URL + string + "+"
+	URL = URL[:-1]
+	return URL
+
+def find_year(raw_data):
+	mapping = {}
+	max_value = 0
+	for text in raw_data:
+		table = str.maketrans(dict.fromkeys(string.punctuation))
+		parsed_text = text[0].translate(table)   
+		for word in parsed_text.split(" "):
+			if word.isdigit():
+				mapping[int(word)] = text[1]
+				if int(word) > max_value:
+					max_value = int(word)
+				break
+	if max_value == 0:
+		return raw_data[0][1]
+				
+	return mapping, max_value
+
+def get_h5_index(URL):
+	r = requests.get(URL)
+	soup = BeautifulSoup(r.content,  "html.parser")
+	raw_data = []
+	for a in soup.find_all('a', href=True):
+		if "journalsearch" in a["href"]:
+			raw_data.append([a.span.text, a["href"]])
+
+	mapping, max_value = find_year(raw_data)
+	url = "https://www.scimagojr.com/" + mapping[max_value]
+	r = requests.get(url)
+	soup = BeautifulSoup(r.content,  "html.parser")
+	html = soup.find(class_ = "hindexnumber")
+	return html.text
 
 def parse_recs(recs):
 	papers = recs.split("\n")
@@ -24,45 +76,77 @@ def parse_recs(recs):
 	return papers
 
 def extract_id(URL):
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content,  "html.parser")
-    
-    id = soup.find(class_ = "list-title is-inline-block").text[6:16]
-    return id
+	r = requests.get(URL)
+	soup = BeautifulSoup(r.content,  "html.parser")
+	
+	id = soup.find(class_ = "list-title is-inline-block").text[6:16]
+	return id
 
 def extract_conference(id): 
-    URL = f"https://arxiv.org/abs/{id}"
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content,  "html.parser")
-    html = soup.find(class_ = "tablecell comments mathjax")
-    # html = soup.find(class_ = "arxivid")
-    if html:
-        conf = html.text
-    else:
-        conf = "No conference found in arxiv"
-    return conf
+	URL = f"https://arxiv.org/abs/{id}"
+	r = requests.get(URL)
+	soup = BeautifulSoup(r.content,  "html.parser")
+	html = soup.find(class_ = "tablecell comments mathjax")
+	# html = soup.find(class_ = "arxivid")
+	if html:
+		conf = html.text
+	else:
+		conf = "No conference found in arxiv"
+	return conf
 
-# def parse_conf(conf):
-# 	return None
+def find_year(raw_data):
+	mapping = {}
+	max_value = 0
+	for text in raw_data:
+		table = str.maketrans(dict.fromkeys(string.punctuation))
+		parsed_text = text[0].translate(table)   
+		for word in parsed_text.split(" "):
+			if word.isdigit():
+				mapping[int(word)] = text[1]
+				if int(word) > max_value:
+					max_value = int(word)
+				break
+	if max_value == 0:
+		return raw_data[0][1]
+				
+	return mapping, max_value
 
-# def parse_h5(h5):
-# 	return None
+def generate_prompt(conf):
+	# prompt = "Can you extract the only conference from these strings: " 
+	# for conf in confs:
+	#     prompt = prompt + f"\'{conf}\'" + ", "
+	# prompt = prompt[:-2]
+	
+	# prompt += " without the date, any form of abbreviation, or workshop"
 
-# def parse_impact(impact):
-# 	return None
+	few_shot_prompt = f"Extract the main conference from the corresponding texts below without any numbers or abbreviations. \n\
+	Text 1: Proceedings of the 46th ACM Symposium on Theory of Computing (STOC 2014), pp. 283-292 (2014) \n\
+	Conference 1: Symposium on Theory of Computing \n\n\
+	Text 2: The paper has been accepted by IEEE Transactions on Cognitive Communications and Networking \n\
+	Conference 2: Transactions on Cognitive Communications and Networking \n\n\
+	Text 3: Accepted by the ACM Transactions on Intelligent Systems and Technology (TIST) \n\
+	Conference 3: Transactions on Intelligent Systems and Technology \n\n\
+	Text 4: Proceedings of the Interdisciplinary Workshop on Human-Drone Interaction co-located with the 2020 ACM CHI Conference on Human Factors in Computing Systems (CHI 2020) - this http URL \n\
+	Conference 4: Conference on Human Factors in Computing Systems \n\n\
+	Text 5: {conf} \n\
+	Conference 5: "
+	
+	return few_shot_prompt
 
 
-def make_df(papers, confs):
+
+
+def make_df(papers, confs, h5_indices):
 	# conf = ["test", "test", "test"]
-	sample_h5 = [53, 34, 23]
 	sample_impact = [5, 8, 6]
 	
-	chart = pd.DataFrame({"Conference": confs, "Related Paper": papers, "H5-index": sample_h5, "Impact Score": sample_impact})
+	chart = pd.DataFrame({"Conference": confs, "Related Paper": papers, "Most Recent H5-index": h5_indices, "Impact Score": sample_impact})
 	# chart["Publish Date"] = pd.to_datetime(chart["Publish Date"])
-	chart_1 = chart.sort_values("H5-index", ascending=False)
-	chart_2 = chart.sort_values("Impact Score", ascending=False)
+	chart_1 = chart.sort_values("Most Recent H5-index", ascending=False)
+	# chart_2 = chart.sort_values("Impact Score", ascending=False)
+	bar_chart = px.bar(chart, x="Most Recent H5-index", y="Conference", orientation="h")
 
-	return chart_1, chart_2
+	return chart_1, bar_chart
 
 
 
@@ -84,32 +168,31 @@ if st.button("Get similar papers"):
 	ids = [extract_id(url) for url in URLs]
 	confs = [extract_conference(id) for id in ids]
 
-	chart_1, chart_2 = make_df(papers, confs)
-	st.markdown("Recommended Conference Based on H5-index")
-	st.dataframe(chart_1)
-	st.markdown("Recommended Conference Based on Impact Score")
-	st.dataframe(chart_2)
+	parsed_confs = []
 
+	for conf in [confs[0]]:
+		prompt = generate_prompt(conf)
+		inputs = {"text": prompt}
+		# res = requests.post(url="http://0.0.0.0:8000/get_main_confs", json=inputs)
+		# print(res.text)
+		# parsed_confs.append(res.text)
+	
+	parsed_confs = [s.strip() for s in ["International Congress on Sound and Vibration",
+					"International Conference on Service Oriented Computing",
+					"Conference on Human Factors in Computing Systems"]]
+
+
+	h5_indices = []
+	for parsed_conf in parsed_confs:
+		conf_url = generate_conf_url(parsed_conf)
+		h5_index = get_h5_index(conf_url)
+		h5_indices.append(int(h5_index))
+
+	chart_1, bar_chart = make_df(papers, parsed_confs, h5_indices)
+	st.markdown("Recommended Conference Based on Related Papers and H5-index")
+	st.dataframe(chart_1[["Conference", "Related Paper"]])
+	st.plotly_chart(bar_chart)
 
 
 
 #drone type classification using time series data
-
-
-# text_input = st.text_input("Enter some text")
-# cand_labels = st.text_input("Candidate labels")
-
-
-# # Get a prediction
-# # sequence_to_classify = "one day I will see the world"
-# # candidate_labels = ['travel', 'cooking', 'dancing']
-# # labels = cand_labels.split(" ")
-
-# inputs = {"text_input": text_input, "candidate_labels": cand_labels}
-
-# if st.button("Get most likely label"):
-# 	res = requests.post(url="http://0.0.0.0:8000/predict_sentiment", json=inputs)
-# 	predictions = json.loads(res.text)
-# 	sequence = predictions["labels"][np.argmax(predictions["scores"])]
-
-# 	st.subheader(f"{sequence}")
